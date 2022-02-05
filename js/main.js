136d9bfca6b2f956996d3696ceeb6f04263623fe
zhjrcc <zjrhello@gmail.com>
2022-02-06 00:04:09 +0800

    Site updated: 2022-02-06 00:04:08

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 if (/mobile/i.test(navigator.userAgent) || /android/i.test(navigator.userAgent)) {
   document.body.classList.add('mobile')
 }
+hljs.initHighlightingOnLoad();
+console.log('hello world')
